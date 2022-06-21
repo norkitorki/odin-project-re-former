288c0ac110e35e5170577638f73809de4deb2a51
norkitorki <norki@tuta.io>
2022-06-21 10:36:49 +0200

    Add create action logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
