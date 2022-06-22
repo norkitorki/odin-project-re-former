80a35237c7f232cd01f59177c6113548d6d77d5a
norkitorki <norki@tuta.io>
2022-06-22 07:37:43 +0200

    Define private method set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,10 @@ class UsersController < ApplicationController
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
